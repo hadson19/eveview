34b70acfd38702f0f51c5fd86bb0b8d12ce4af15
foxjazz <foxr4@foxjazz.net>
2016-11-19 10:07:47 -0700

    breaks on Observable.interval  not sure why

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import {BitService} from './bitservice';
 import {IData} from "./bit";
 import 'rxjs/add/observable/interval';
 import {Observable} from "rxjs";
+import 'rxjs/add/operator/share';
+
 
 @Component({
     selector: 'as-home',
@@ -16,45 +18,53 @@ import {Observable} from "rxjs";
 export class HomeComponent  implements OnInit {
     constructor(bs: BitService){
         this.bitService = bs;
+        /*Observable.interval(1000)
+            .take(17000).map((x) => x+1)
+            .subscribe((x) => {
+                this.getData();
+            }).share();*/
     }
     private bitService;
     public obj;
-    public cryptice: IData;
+    public cryptice = {"BTC":0.01292,"USD":1.67,"EUR":1.15};
     public BTC;
     public USD;
     public EUR;
     public xs;
     //private datatimer: Observable<Object>;
     public test(){
-        let t = this.cryptice;
-        this.BTC = this.cryptice.BTC;
-        this.USD = this.cryptice.USD;
-        this.EUR = this.cryptice.EUR;
-
-        this.xs = 'test me';
+        this.getData();
 
     }
     private getData(){
 
-
-
+        Observable.timer(0, 2000).flatMap(_ => {
+            return this.bitService.getData();
+        }).subscribe(res2 => {
+            this.cryptice = res2;
+            this.BTC = this.cryptice.BTC;
+            this.USD = this.cryptice.USD;
+            this.EUR = this.cryptice.EUR;
+        }, err => console.log('Something went wrong: ' + err.message));
+/*
             this.bitService.getData().subscribe(res2 => {
                 this.cryptice = res2;
                 this.BTC = this.cryptice.BTC;
                 this.USD = this.cryptice.USD;
                 this.EUR = this.cryptice.EUR;
             }, err => console.log('Something went wrong: ' + err.message));
+*/
 
     }
     ngOnInit()
     {
         this.getData();
         //Observable.interval(1000).subscribe(data => { this.getData();});
-        let obs = Observable.interval(500).take(5)
+      /*  let obs = Observable.interval(500).take(5)
             .do(i => console.log("obs value "+ i) );
 
         obs.subscribe(value => console.log("observer 1 received " + value));
 
-        obs.subscribe(value => console.log("observer 2 received " + value));
+        obs.subscribe(value => console.log("observer 2 received " + value));*/
     }
 }

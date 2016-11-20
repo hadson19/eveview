276dbd65d0886d9db649771c39440cf1770a80a3
foxjazz <foxr4@foxjazz.net>
2016-11-19 21:23:32 -0700

    price spread done

--- src/app/ItemsByGroup/ItemTypes.ts
+++ src/app/ItemsByGroup/ItemTypes.ts
@@ -5,7 +5,8 @@
 
 export interface ItemTypesA {items: Array<ItemType>;}
 export interface ItemTypes { totalCount_str: string; pageCount: number; items: Array<ItemType>; }
-export interface ItemType { marketGroup: MarketGroup; type: Type; id: number; id_str: string; Jitaprice: number; Alertprice: number; AlertCondition: string; }
+export interface ItemType { marketGroup: MarketGroup; type: Type; id: number; id_str: string; Jitaprice: number; Amarrprice: number;
+Hekprice:number; Dodixieprice: number; Rensprice: number; Tashprice: number; Ourprice: number; Alertprice: number; AlertCondition: string; }
 export interface MarketGroup {href: string; id: number; id_str: string; }
 export interface Type { id_str: string; href: string; id: number; name: string; icon: Icon;}
 export interface Icon {href: string; }
